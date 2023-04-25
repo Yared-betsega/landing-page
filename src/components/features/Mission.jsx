b7fb7f4482518f48cdb3cf6eb273f209c75abc55
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-04-25 14:29:30 +0300

    add pricing page

--- src/components/features/Mission.jsx
+++ src/components/features/Mission.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Mission({headerText, descriptionText}) {
     return (
-        <div className='mt-28 mb-20 mx-80 px-20 font-Montserrat'>
+        <div className='mt-28 mb-20 mx-80 px-20 font-Montserrat text-center'>
             <h1 className='text-5xl text-black font-bold'>{headerText}</h1>
             <p className='capitalize text-2xl font-normal mt-10 text-center'>{descriptionText}</p>
         </div>
--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import Mission from '../features/Mission'
+import PriceCard from './Price_Card'
 
 export default function Pricing() {
-  return (
-    <div>
-        <Mission headerText={"Pricing"} descriptionText={"we offer a range of pricing options, designed to meet the diverse needs of out clients"}></Mission>
-        
-    </div>
-  )
+    const socialAccountActions = [
+        "Access to Analytics (premium only)",
+        "Priority Customer Support (premium only)"
+    ];
+    const professionalAccountActions = [
+        "access to analytics",
+        "Instant job Matching",
+        "Priority Customer Support"
+    ];
+    const businessAccountActions =[
+        "Access to lead Generation tools",
+        "Unlimited Business Promotions",
+        "priority customer support"
+    ];
+    const jointAccountActions = [
+        "unlimited access to analytics, job connector, lead generation tool, digital magazine",
+        "unlimited business promotion",
+        "priority customer support"
+    ]
+
+    return (
+        <div>
+            <Mission headerText={"Pricing"} descriptionText={"we offer a range of pricing options, designed to meet the diverse needs of out clients"}></Mission>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-28 px-20 py-5'>
+                <PriceCard type={"Social"} price={8} description={"It offers a reliable and cost-effective hosting solution with all the essentials you need to get started."} actions={socialAccountActions} url={"#"}></PriceCard>
+                <PriceCard type={"Professional"} price={10} description={"It offers a reliable and cost-effective hosting solution with all the essentials you need to get started."} actions={professionalAccountActions} url={"#"}></PriceCard>
+                <PriceCard type={"Business"} price={12} description={"It offers a reliable and cost-effective hosting solution with all the essentials you need to get started."} actions={businessAccountActions} url={"#"}></PriceCard>
+                <PriceCard type={"Joint"} price={18} description={"It offers a reliable and cost-effective hosting solution with all the essentials you need to get started."} actions={jointAccountActions} url={"#"}></PriceCard>
+            </div>
+        </div>
+    )
 }

47a2b30409bc02f35e0c2b67167164e0dfcff429
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-04-26 15:12:13 +0300

    Add icons to accordion

--- src/components/FAQ/Question.jsx
+++ src/components/FAQ/Question.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { Collapse } from "react-collapse";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 export default function Question({ open, toggle, title, desc }) {
   return (
     <div className="pt-[10px]">
       <div
-        className="bg-white py-[25px] px-[50px] flex justify between items-center cursor-pointer"
+        className="bg-white py-[25px] px-32 flex justify-between items-center cursor-pointer"
         onClick={toggle}
       >
         <p className="text-[22px] font-semibold">{title}</p>
         <div className="text-[30px]">
           {open ? (
-            <AiOutlineMinus></AiOutlineMinus>
+            <AiFillCaretUp></AiFillCaretUp>
           ) : (
-            <AiOutlinePlus></AiOutlinePlus>
+            <AiFillCaretDown></AiFillCaretDown>
           )}
         </div>
       </div>

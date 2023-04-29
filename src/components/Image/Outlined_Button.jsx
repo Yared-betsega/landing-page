a4f38e42f31a7c8d7dbf1c029234c049d34f809f
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-04-29 11:08:18 +0300

    Update upper buttons

--- src/components/Image/Outlined_Button.jsx
+++ src/components/Image/Outlined_Button.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function OutlinedButton({ text, shallow }) {
   return (
-    <div className="flex">
-      <button
+  
+    <div className="flex  w-full">
+      <a
+        href={"#"}
         style={{ textAlign: "end" }}
-        className={`${shallow ? 'bg-transparent text-[#DAA624] ' : 'bg-[#DAA624] '} px-5 lg:px-14 py-4 lg:py-5 ml-5 font-semibold text-xs md:text-base lg:text-xl border border-[#DAA624] tracking-wide transition-colors duration-300 transform  rounded-2xl hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80`}
+        className={`${
+          shallow ? "bg-transparent text-[#DAA624] px-5 lg:px-10 " : "bg-[#DAA624] px-5 lg:px-14 "
+        } w-full py-5 lg:py-6 xl-py-6  flex justify-center items-center font-semibold text-xs md:text-base lg:text-2xl tracking-wide border border-[#DAA624] transition-colors duration-300 transform rounded-2xl hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80`}
       >
         {text}
-      </button>
+      </a>
     </div>
   );
 }
--- src/components/Image/image.jsx
+++ src/components/Image/image.jsx
@@ -8,7 +8,7 @@ export default function Image() {
         class="object-cover w-full  relative h-[] object-right"
         src={image}
       />
-      <div className="absolute  w-auto mx-44 mx-10 sm:mx-40 lg:mx-60 mt-2 sm:mt-2 md:mt-3 lg:mt-10 xl:mt-6 2xl:mt-40 object-cover object-right">
+      <div className="absolute  w-auto mx-10  md:mx-28 xl:mx-60 mt-40  object-cover object-right">
         <div className="flex flex-col font-Montserrat">
           <h1
             className="tracking-wide text-xl sm:text-2xl md:text-3xl xl:text-5xl leading-loose sm:leading-normal md:leading-normal xl:leading-normal capitalize text-white font-semibold"
@@ -16,12 +16,12 @@ export default function Image() {
             empowering undeserved market with digital skill and career
             opportunity{" "}
           </h1>
-          <p className="text-white text-base md:text-xl lg:text-2xl my-10 font-light">
+          <p className="text-white text-base md:text-xl lg:text-2xl my-10 font-medium">
             We are committed to helping millennial's, graduates small business
             owners, and entrepreneurs realize their potential and achieve
             success.
           </p>
-          <div className="flex items-center justify-center mt-20">
+          <div className="flex items-center justify-center mt-20 gap-10 mx-0 md:mx-20 lg:mx-28 xl:mx-40 2xl:mx-56">
             <OutlinedButton text={"Get Started"} shallow={false}></OutlinedButton>
             <OutlinedButton text={"How it Works"} shallow={true}></OutlinedButton>
           </div>
